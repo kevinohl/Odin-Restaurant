69227db3c4840b1f9e41337fc21270cd032779c7
Kevin Ohl <kevinohl@msn.com>
2023-04-12 10:00:05 +0200

    Set up css minimization

--- src/home.js
+++ src/home.js
@@ -1,5 +1,9 @@
+// Each module will export a function that creates a div element, 
+// adds the appropriate content and styles to that element and then 
+// appends it to the DOM.
+
 function generateHome() {
-    return 0;
+  return 0;
 }
 
 export default generateHome;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -7,10 +9,23 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
-  rules: [
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
-  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
+    ],
+    minimize: true,
+  },
 };
+
+
+  
